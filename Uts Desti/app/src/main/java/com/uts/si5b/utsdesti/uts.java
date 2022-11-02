93aa1a47a6f013366f9d20838782767996bc964e
Destirahmantheja <destitheja43742@gmail.com>
2022-11-02 11:21:03 +0700

    UTS PAB

---
+++ Uts Desti/app/src/main/java/com/uts/si5b/utsdesti/uts.java
@@ -0,0 +1,14 @@
+package com.uts.si5b.utsdesti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class uts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uts);
+    }
+}
